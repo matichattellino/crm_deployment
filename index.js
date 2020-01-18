e45a7c9ff31e41355b624b6763fa73a7665ac9eb
matias chattellino <matiaschattellino@gmail.com>
2020-01-17 21:01:06 -0300

    algunos ajustes antes de deployment

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.DB_URL, {
 //crear el servidor
 const app = express();
 
+//carpeta publica
+app.use(express.static('uploads'));
+
 //habilitar el body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -41,8 +44,6 @@ app.use(cors(corsOptions));
 //rutas de la app
 app.use('/', routes());
 
-//carpeta publica
-app.use(express.static('uploads'));
 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
